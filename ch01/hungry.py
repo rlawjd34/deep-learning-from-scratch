d56e84a13b073d12c6d259f1bd43bd44ea481bc5
rlawjd34 <rlawjd28@naver.com>
2020-03-04 23:09:07 +0900

    아마존 EC2 ap-northeast-2 ip 추출

--- ch01/hungry.py
+++ ch01/hungry.py
@@ -1,1 +1,8 @@
 print("I'm hungry!")
+import requests
+
+rj = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()
+
+ips = [prefix['ip_prefix'] for prefix in rj['prefixes'] if prefix['service'] == 'EC2' and prefix['region'] == 'ap-northeast-2']
+
+print(', '.join(ips))
